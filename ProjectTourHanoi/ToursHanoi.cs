9a58defe58518d904b2885549fbacd05efd8f34c
Alexis Young <alexyoung@outlook.fr>
2023-02-12 11:09:51 -0500

    Ajout de commentaires

--- ProjectTourHanoi/ToursHanoi.cs
+++ ProjectTourHanoi/ToursHanoi.cs
@@ -8,6 +8,7 @@ namespace ProjectTourHanoi
         private Tour[] _tours = new Tour[3];
         private int _nbAnneau;
 
+        //Constructeur
         public ToursHanoi(int nbAnneau)
         {
             _nbAnneau = nbAnneau;
@@ -17,39 +18,47 @@ namespace ProjectTourHanoi
             reinitialiser();
         }
 
+        //Réinitialise le jeu
         public void reinitialiser()
         {
+            //Vide les tours
             // _tours[0].clear();
             // _tours[1].clear();
             // _tours[2].clear();
 
+            //Ajout des anneaux sur la tour A
             for (int i = _nbAnneau; i > 0; i--)
             {
                 //_tours[0].push(i);
             }
         }
 
+        //Déplace lun anneau si le mouvement est légal
         private bool deplacer(string strDe, string strVers)
         {
-            int nb;
-            int de;
-            int vers;
+            int nb; //Variable de l'anneau à déplacer
+            int de; //Variable de la tour de départ
+            int vers; //Varibale de la tour de fin
 
+            //Récupération des indice de tours (a = 0, b = 1, c = 2)
             de = transform(strDe);
             vers = transform(strVers);
             
-            /*
+            /*//Vérifie si l'anneau a déplacer est plus petit
             if (_tours[de].peek() < _tours[vers].peek())
             {
+                //Déplacement de l'anneau
                 nb = _tours[de].peek();
                 _tours[de].pop();
                 _tours[vers].push(nb);
                 
+                //Affichage du déplacement
                 Console.WriteLine("L'anneau de diamètre " + nb + " est déplacé de la tour " + strDe + " vers la tour " + strVers);
                 return true;
             }
             else
             {
+                //Affichage du message d'erreur
                 Console.WriteLine("Déplacement illégal");
                 return false;
             }
@@ -58,31 +67,37 @@ namespace ProjectTourHanoi
             return false;
         }
 
+        //Transforme les lettres des tour en indice int (a = 0, b = 1, c = 2)
         private int transform(string de)
         {
             int nb = -1;
             bool fin = false;
 
+            //Boucle qui prend fin lors d'un résultat valide
             while (!fin)
             {
+                //Si la tour est a, retourne 0 et sort de la boucle
                 if (de.Equals("a") || de.Equals("A"))
                 {
                     nb = 0;
                     fin = true;
                 }
 
+                //Si la tour est b, retourne 1 et sort de la boucle
                 else if (de.Equals("b") || de.Equals("B"))
                 {
                     nb = 1;
                     fin = true;
                 }
 
+                //Si la tour est c, retourne 2 et sort de la boucle
                 else if (de.Equals("c") || de.Equals("C"))
                 {
                     nb = 2;
                     fin = true;
                 }
 
+                //Si la tour est non-valide, affiche le message d'erreur et reste dans la boucle
                 else
                 {
                     Console.WriteLine("Choix de tour non-valide");
